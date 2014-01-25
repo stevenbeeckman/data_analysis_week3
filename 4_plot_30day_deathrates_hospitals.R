9dad18cdb9ac4f97eb1609cef8751ce15ca672a2
Steven Beeckman <steven.beeckman@gmail.com>
2014-01-25 18:45:29 +0100

    Here's how to join two data sets: use merge().

--- 4_plot_30day_deathrates_hospitals.R
+++ 4_plot_30day_deathrates_hospitals.R
@@ -1,4 +1,7 @@
 plot_30day_deathrates_number_of_patients = function{
 	outcome = read.csv("outcome-of-care-measures.csv", colClasses = "character")
 	hospital = read.csv("hospital-data.csv", colClasses = "character")
+
+	# join the two datasets together to math the Hospital.Ownership variable to the death rate data
+	outcome.hospital = merge(outcome, hospital, by = "Provider.Number")
 }
