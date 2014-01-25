a9cba58e830a2e452b34c70e62db225a3ca4d1bd
Steven Beeckman <steven.beeckman@gmail.com>
2014-01-25 16:09:21 +0100

    Added vertical line on median.

--- 2_plot_30day_mortalityrates_heartattack_heartfailure_pneumonia.R
+++ 2_plot_30day_mortalityrates_heartattack_heartfailure_pneumonia.R
@@ -14,8 +14,14 @@ plot_30day_mortalityrates_heartattack_heartfailure_pneumonia = function(){
 	hist_max = max(max(range(outcome[,11], na.rm=T)), max(range(outcome[,17], na.rm=T)), max(range(outcome[,23], na.rm=T)))
 	print(hist_min)
 	print(hist_max)
+	print(paste("Median for heart attack:", median(outcome[,11], na.rm=T)))
+	print(paste("Median for heart failure:", median(outcome[,17], na.rm=T)))
+	print(paste("Median for pneumonia:", median(outcome[,23], na.rm=T)))
 	hist(outcome[, 11], main="Heart Attack", xlab="30-day Death Rate", xlim=c(hist_min, hist_max))
+	abline(v=median(outcome[,11], na.rm=T))
 	hist(outcome[, 17], main="Heart Failure", xlab="30-day Death Rate", xlim=c(hist_min, hist_max))
+	abline(v=median(outcome[,17], na.rm=T))
 	hist(outcome[, 23], main="Pneumonia", xlab="30-day Death Rate", xlim=c(hist_min, hist_max))
-
+	abline(v=median(outcome[,23], na.rm=T))
+	
 }
