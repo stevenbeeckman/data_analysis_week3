e73ba1a9def60951d0449503a73f886969a41407
Steven Beeckman <steven.beeckman@gmail.com>
2014-01-25 18:44:04 +0100

    Initial 4th part of the assignment.

---
+++ 4_plot_30day_deathrates_hospitals.R
@@ -0,0 +1,4 @@
+plot_30day_deathrates_number_of_patients = function{
+	outcome = read.csv("outcome-of-care-measures.csv", colClasses = "character")
+	hospital = read.csv("hospital-data.csv", colClasses = "character")
+}
