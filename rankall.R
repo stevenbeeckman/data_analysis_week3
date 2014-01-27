36ea95734a28304524c4b452c2407b7f8576cfcb
Steven Beeckman <steven.beeckman@gmail.com>
2014-01-28 00:18:53 +0100

    Final submissions.

--- rankall.R
+++ rankall.R
@@ -1,3 +1,63 @@
-rankall = function(){
-	
+rankall = function(outcome, num = "best"){
+	## read outcome data
+	outcomes = read.csv("outcome-of-care-measures.csv", colClasses = "character")
+	## check that state and outcome are valid
+	validate(outcome, num)
+	## for each state, find the hospital of the given rank
+	states = sort(unique(outcomes$State))
+	# print(states)
+	hospitals = sapply(states, findHospitalWithDeathRateRanking, outcome, num)
+	# print(str(hospitals))
+	# print(hospitals)
+	result = data.frame(hospital = hospitals, state = states, stringsAsFactors = F)
+	# str(result)
+	result
+	## return a data frame with the hospital names and the (abbreviated) state name
+}
+
+validate = function(outcome, num){
+	if(outcome != "heart attack" && outcome != "heart failure" && outcome != "pneumonia"){
+		stop("invalid outcome")
+	}
+}
+
+findHospitalWithDeathRateRanking = function(state, outcome, num){
+	stateOutcomesWithNA = subset(outcomes, State == state)
+	stateOutcomes = subset(stateOutcomesWithNA, complete.cases(stateOutcomesWithNA))
+
+	if(outcome == "heart attack"){
+		orderedStatesIndex = order(stateOutcomes[,11])
+		orderedStateOutcomes = stateOutcomes[orderedStatesIndex,]
+		if(num == "best"){
+			num = 1
+		}else if(num == "worst"){
+			num = length(orderedStateOutcomes[,11])
+		}else if(num > length(orderedStateOutcomes[,11])){
+			NA
+		}
+		orderedStateOutcomes[num, "Hospital.Name"]
+	}else if(outcome == "heart failure"){
+		orderedStatesIndex = order(stateOutcomes[,17])
+		orderedStateOutcomes = stateOutcomes[orderedStatesIndex,]
+		if(num == "best"){
+			num = 1
+		}else if(num == "worst"){
+			num = length(orderedStateOutcomes[,17])
+		}else if(num > length(orderedStateOutcomes[,17])){
+			NA
+		}
+		orderedStateOutcomes[num, "Hospital.Name"]
+	}else if(outcome == "pneumonia"){
+		orderedStatesIndex = order(stateOutcomes[,23])
+		orderedStateOutcomes = stateOutcomes[orderedStatesIndex,]
+		if(num == "best"){
+			num = 1
+		}else if(num == "worst"){
+			num = length(orderedStateOutcomes[,23])
+		}else if(num > length(orderedStateOutcomes[,23])){
+			NA
+		}
+		orderedStateOutcomes[num, "Hospital.Name"]
+	}
+
 }
--- rankhospital.R
+++ rankhospital.R
@@ -1,3 +1,59 @@
-rankhospital = function(state, outcome, num){
-	
+rankhospital = function(state, outcome, num = "best"){
+	## read outcome data
+	outcomes = read.csv("outcome-of-care-measures.csv", colClasses = "character")
+
+	## check that state and outcome are valid
+	validate(state, outcome, num)
+
+	## return hospital name in that state with lowest 30-day death rate
+	findHospitalWithDeathRateRanking(state, outcome, num)
+}
+
+validate = function(state, outcome, num){
+	if(outcome != "heart attack" && outcome != "heart failure" && outcome != "pneumonia"){
+		stop("invalid outcome")
+	}else if(!(state %in% outcomes$State)){
+		stop("invalid state")
+	}
+}
+
+findHospitalWithDeathRateRanking = function(state, outcome, num){
+	stateOutcomesWithNA = subset(outcomes, State == state)
+	stateOutcomes = subset(stateOutcomesWithNA, complete.cases(stateOutcomesWithNA))
+
+	if(outcome == "heart attack"){
+		orderedStatesIndex = order(stateOutcomes[,11])
+		orderedStateOutcomes = stateOutcomes[orderedStatesIndex,]
+		if(num == "best"){
+			num = 1
+		}else if(num == "worst"){
+			num = length(orderedStateOutcomes[,11])
+		}else if(num > length(orderedStateOutcomes[,11])){
+			NA
+		}
+		print(paste(orderedStateOutcomes[num, "Hospital.Name"], orderedStateOutcomes[num,11]))
+	}else if(outcome == "heart failure"){
+		orderedStatesIndex = order(stateOutcomes[,17])
+		orderedStateOutcomes = stateOutcomes[orderedStatesIndex,]
+		if(num == "best"){
+			num = 1
+		}else if(num == "worst"){
+			num = length(orderedStateOutcomes[,17])
+		}else if(num > length(orderedStateOutcomes[,17])){
+			NA
+		}
+		print(paste(orderedStateOutcomes[num, "Hospital.Name"], orderedStateOutcomes[num,17]))
+	}else if(outcome == "pneumonia"){
+		orderedStatesIndex = order(stateOutcomes[,23])
+		orderedStateOutcomes = stateOutcomes[orderedStatesIndex,]
+		if(num == "best"){
+			num = 1
+		}else if(num == "worst"){
+			num = length(orderedStateOutcomes[,23])
+		}else if(num > length(orderedStateOutcomes[,23])){
+			NA
+		}
+		print(paste(orderedStateOutcomes[num, "Hospital.Name"], orderedStateOutcomes[num,23]))
+	}
+
 }
