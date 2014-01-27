2a6bc2159af8f36d5bf96ab71964926bef084284
Steven Beeckman <steven.beeckman@gmail.com>
2014-01-27 19:08:39 +0100

    Lattice panel magic.

--- 4_plot_30day_deathrates_hospitals.R
+++ 4_plot_30day_deathrates_hospitals.R
@@ -1,7 +1,17 @@
-plot_30day_deathrates_number_of_patients = function{
+plot_30day_deathrates_number_of_patients = function() {
 	outcome = read.csv("outcome-of-care-measures.csv", colClasses = "character")
 	hospital = read.csv("hospital-data.csv", colClasses = "character")
 
 	# join the two datasets together to math the Hospital.Ownership variable to the death rate data
 	outcome.hospital = merge(outcome, hospital, by = "Provider.Number")
+
+	death = as.numeric(outcome.hospital[, 11])
+	npatient = as.numeric(outcome.hospital[, 15])
+	owner = factor(outcome.hospital$Hospital.Ownership)
+
+	library(lattice) # needed for xyplot
+	xyplot(death ~ npatient | owner, panel = function(x,y, ...){ panel.xyplot(x, y) 
+		panel.lmline(x, y)}, main="Heart Attack 30-day Death Rate by Ownership", xlab = "Number of Patients Seen", ylab = "30-day Death Rate")
+	# p2 = panel.lmline(death, npatient)
+	# p1 + p2
 }
